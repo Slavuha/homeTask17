19a4db1f6b0172cf62b974fa14b6be18d9193536
Mazur <Slavaslidchum@gmail.com>
2022-04-09 10:16:28 +0300

    go

--- src/test/java/testApi.java
+++ src/test/java/testApi.java
@@ -6,30 +6,33 @@ import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 public class testApi {
 @Test
     public void getUrl () {
-    String baseUrl = "https://swapi.dev/api/starships";
+
+    String baseUri = "https://swapi.dev/api";
+    String basePath = "/starships/10";
+    String name = "Millennium Falcon";
     given()
-            .baseUri(baseUrl)
-            .basePath("/10")
+            .baseUri(baseUri)
+            .basePath(basePath)
             .contentType(ContentType.JSON)
             .when().get()
             .then()
             .statusCode(200)
+            .body.("name", hasSize(16))
             .body("name", equalTo("Millennium Falcon"))
             .body("cost_in_credits", equalTo("100000"))
             .body("length", equalTo("34.37"))
-            .body("pilots", equalTo
-                    ("https://swapi.dev/api/people/13/",
+            .body("pilots", hasItems(
+                    "https://swapi.dev/api/people/13/",
                     "https://swapi.dev/api/people/14/",
                     "https://swapi.dev/api/people/25/",
                     "https://swapi.dev/api/people/31/"));
 
 }
-
-
-
 
 }
